2d29d4a32b07ae692f7c409f5a69fce88e241e92
tharu <tharucse@gmail.com>
2022-09-16 20:29:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -19,26 +19,56 @@ let myString="some bad string";
 console.log(myString.filter("bad"));
 
 
-Array.prototype.bubblesort=function()
+Array.prototype.bubbleSort=function()
 {
     let i=0;
-    while(i<this.length-1)
-    {
+   
         let swapped;
         do
         {
             swapped=false;
-            if(this[i]>this[i+1])
+            i=0;
+            while(i<this.length)
             {
-                let temp=this[i+1];
-                this[i+1]=this[i];
-                this[i]=temp;
-                swapped=true;
-            }
+                if(this[i]>this[i+1])
+                {
+                    let temp=this[i+1];
+                    this[i+1]=this[i];
+                    this[i]=temp;
+                    swapped=true;
+                }
+                i++;                           
+           }
        }while(swapped)
-    }
     return this;
+ }
+    
+
+
+let array=[6,4,0, 3,-2,1];
+console.log([6,4,0, 3,-2,1].bubbleSort());
+
+var Person = function() {};
+Person.prototype.initialize = function(name, age)
+{
+ this.name = name;
+ this.age = age;
+}
+Person.prototype.describe = function()
+{
+ return this.name + ", " + this.age + " years old.";
+}
+var Student = function() {};
+Student.prototype = new Person();
+
+
+var Teacher=function(){};
+Teacher.prototype= new Person();
+Teacher.prototype.teach= function(subject)
+{
+    return this.name + " is now teaching " + subject;
 }
 
-let array=[8,9,7];
-console.log(array.bubblesort());
+var teacher1= new Teacher();
+teacher1.initialize("Jen",23);
+console.log(teacher1.teach("Maths"));
