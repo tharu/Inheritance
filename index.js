e3e2ec16f27dc4cb79e299873f99e719122182db
tharu <tharucse@gmail.com>
2022-09-16 19:58:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -17,3 +17,28 @@ String.prototype.filter = function (...bannedWords) {
 
 let myString="some bad string";
 console.log(myString.filter("bad"));
+
+
+Array.prototype.bubblesort=function()
+{
+    let i=0;
+    while(i<this.length-1)
+    {
+        let swapped;
+        do
+        {
+            swapped=false;
+            if(this[i]>this[i+1])
+            {
+                let temp=this[i+1];
+                this[i+1]=this[i];
+                this[i]=temp;
+                swapped=true;
+            }
+       }while(swapped)
+    }
+    return this;
+}
+
+let array=[8,9,7];
+console.log(array.bubblesort());
